1fa24a9f48011c683b08e57a4fd6f170ad0c838e
camilof91 <camilofetecua2011@outlook.com>
2024-05-02 16:21:25 -0500

    add 1 export_to_CSV

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,27 +8,25 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print(f"Usage: {argv[0]} <employee_id>")
-        exit(1)
 
     employee_id = argv[1]
 
     user_response = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    )
+    )#.json()
     todos_response = requests.get(
         f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    )
+    )#.json()
 
     user = user_response.json()
     todos = todos_response.json()
-
+    
     done_tasks = [task for task in todos if task.get("completed")]
 
-    print(
-        f"Employee {user.get('name')} is done with tasks
-        ({len(done_tasks)}/{len(todos)}): ")
+    print(f"Employee {user.get('name')} is done with tasks ({len(done_tasks)}/{len(todos)}): ")
 
     for task in done_tasks:
         print(f"\t{task.get('title')}")
+
+    print(type(user_response))
+    print(user)
